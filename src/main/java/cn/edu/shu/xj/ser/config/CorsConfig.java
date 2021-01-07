633fefdc762f2e9cb7603c343939b102eaa21e53
SmellyCat44 <67810649+SmellyCat44@users.noreply.github.com>
2021-01-07 15:59:39 +0800

    Update CorsConfig.java

--- src/main/java/cn/edu/shu/xj/ser/config/CorsConfig.java
+++ src/main/java/cn/edu/shu/xj/ser/config/CorsConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableLoadTimeWeaving
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
